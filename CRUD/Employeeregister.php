2bdf2cd2d3b5be894e2060dd2ac3d87e9e88e94e
Abimeena <114210843+Abimeenaa@users.noreply.github.com>
2022-11-30 18:42:25 +0530

    CRUD files uploaded

---
+++ CRUD/Employeeregister.php
@@ -0,0 +1,143 @@
+
+<?php 
+include 'connection.php';
+if(isset($_POST['submit'])){
+     $name=$_POST['name'];
+     $email=$_POST['email'];
+    //  $dob=date('Y-m-d',strtotime($_POST['dateofbirth']));
+    $phone=$_POST['phone'];
+    $password=$_POST['password'];
+    $address=$_POST['address'];
+      if(empty($name) || empty($email)|| empty($phone) || empty($password) || empty($address)) 
+      {
+      //  echo "<br>All fields are required. Either one or many fields looks empty.<br>";
+      //  echo "<br/>";
+      echo '<script language="javascript">alert("All fields are required. Either one or many fields looks empty");</script>';  
+       echo "<a href='Employeeregister.php'><h3>Go back</h3></a>";
+     } else
+     {
+      $sql= "INSERT INTO employee(name,email,phone,password,address) VALUES('$name','$email','$phone','$password','$address')";
+      $result = mysqli_query($connection,$sql);
+      if(!$result){
+             die("Query failed".mysqli_error($connection));
+           }
+           else{
+           //echo "Registred successfully";
+           echo '<script language="javascript">alert("Your data has been registered successfully!");</script>';  
+           echo '<script language="javascript">window.location = "login.php";</script>';  
+          // header('location:login.php');
+           echo "<br/>";
+           }
+      $connection->close();
+    }
+  }
+    
+  
+  
+//   } else {
+//     //  $connection = mysqli_connect('localhost','root','','registration_web');
+//     //  if(!$connection){ 
+//     //     die("OOPS!! CONNECTION FAILED!".mysqli_connect_error());
+//     //  }
+//      //echo"CONNECTED SUCCESSFULLY!";
+//      //$query= "SELECT * FROM customerdata"; 
+//      $query = "INSERT INTO customerdata(name,email,dob,phone,password) VALUES('$name','$email','$dob',$phone','$password')";
+//     //     echo "Data inserted successfully";
+//     //  }  one approach to check the query is executed successfully
+//     //  else
+//     //  die("Query failed".mysqli_error($connection));
+//      $result = mysqli_query($connection,$query);
+//      $row = mysqli_fetch_assoc($result);
+//      // another approach
+//      if(!$result){
+//         die("Query failed".mysqli_error($connection));
+//background-color: #4158D0;
+//background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);
+
+
+//      }
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sign-up</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- bootstrap in php -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+  <!-- <link rel="stylesheet" type="text/css" href="register.css"> -->
+<body style="  background: linear-gradient(to top left, #99ffcc 0%, #9999ff 100%); height: 100vh; "> 
+   <!-- <div class=" linear-gradient" style="height: 100vh;"> -->
+<div class="container-fluid "> 
+<div class="col-sm-10">
+
+    <!-- div container fluid and col-xs-4 is used only one time  -->
+<form action="Employeeregister.php" method="post">
+    <div class="container-fluid ">  
+  <h1 style="text-align:center; color:Black;">Employee Registration Form</h1></div>
+    <!-- <marquee class="blink" behaviour="scroll" width="100%"  direction="right" height="50px" scrollamount="7" >You can Register here</marquee> -->
+    </div>
+    </div>
+    <div class="container-fluid"> 
+    <div class="col-sm-10">
+        <form class="Register" >
+  
+ 
+        <div class="form-group">   
+            <label for="Name"> Name </label><br>
+            <input type="Text" id="Name" name="name" maxlength="20" class="form-control" background-color:#000 placeholder="Enter your name" autocomplete = "off" required/>
+            </div>
+
+            <div class="form-group"> 
+               <label for="Email">Email</label><br>
+                <input type="email" id="email" name="email" maxlength="40" class="form-control"  placeholder="Enter your Mail id" autocomplete = "off" required  />
+                </div>
+
+            <div class="form-group"> 
+               <label for="Dateofbirth">DOB</label><br>
+                <input type="date" id="dob" name="dateofbirth" maxlength="50" class="form-control"  placeholder="Enter your DOB" autocomplete = "off" />
+                </div>
+                
+                <div class="form-group"> 
+                <label for="Gender">Gender</label><br>
+
+                <input type="radio" id="gender" name="radio"  value="Male"/>
+                <label for="radio">Male</label>
+                <input type="radio" id="gender" name="radio" value="Female"/>
+                <label for="radio">Female </label>
+                <input type="radio" id="gender" name="radio" value="other"/>
+                <label for="radio">Other</label>
+                 </div>
+                 
+                <div class="form-group"> 
+                    <label for="Mobileno">PhoneNo</label><br>
+                    <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" maxlength="10" class="form-control"  placeholder="Enter your PhoneNumber" autocomplete = "off" />
+                    </div>
+ 
+                        <div class="form-group"> 
+                            <label for="password">Password</label><br>
+                            <input type="password" id="password" name="password" maxlength="10" class="form-control"  placeholder="Enter your password" autocomplete = "off" />
+                            </div>
+
+                            <div class="form-group">
+                              <label for="Address">Address</label><br>
+                              <textarea rows="5" cols="80" name="address" class="form-control" placeholder="Enter your address here..."></textarea>  
+                              </div>
+
+                                <button type="submit" name="submit" class="btn btn-primary" >Submit</button>
+                                <button type="submit" name="reset" class="btn btn-danger"> Reset</button> 
+                                </div>
+</div>
+              </form>
+</form>
+
+</body>
+</html>
---
+++ CRUD/display.php
@@ -0,0 +1,76 @@
+
+
+<?php 
+include"connection.php"
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" >
+        <!-- <link rel="stylesheet" type="text/css" href="register.css"> -->
+        <title>Display page</title>
+        <!-- <marquee class="blink" behaviour="scroll" width="100%"  direction="right" height="30px" scrollamount="7" href="login.css" >You can login here</marquee> -->
+    </head>
+<body style=" background: linear-gradient(to top left, #99ffcc 0%, #9999ff 100%); height: 100vh; "> 
+<!-- <div class="d-flex align-items-center height:100vh;"> -->
+<div class="container-fluid"> 
+<div class="d-flex justify-content-center">
+<div class="col-sm-10">
+<caption><legend style="text-align:center">List of Employee details</legend></caption>
+
+<table class="table table-striped table-hover table-bordered">
+  <thead class="table-dark">
+    <tr>
+    <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Mobileno</th>
+      <th scope="col">Password</th>
+      <th scope="col">Address</th>
+      <th scope="col" style="text-align:center" colspan = "2">Operations</th>  
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $sql="SELECT * FROM employee";
+    $result=mysqli_query($connection,$sql);
+    if($result){
+      while($row=mysqli_fetch_array($result)) {    
+      $Id=$row['Id'];
+      $Name=$row['Name'];
+      $Email=$row['Email'];
+      $phone=$row['phone'];
+      $password=$row['password'];
+      $address=$row['Address'];
+
+      echo '<tr>
+      <th scope="row">'.$Id.'</th>
+      <td>'.$Name.'</td>
+      <td>'.$Email.'</td>
+      <td>'.$phone.'</td>
+      <td>'.$password.'</td>
+      <td>'.$address.'</td>
+      
+     <td> <button type="submit" name="update" class="btn btn-primary"><a href="update.php?updateid='.$Id.'" class="text-light">Update</a></button>
+     <button  type="submit" name="Delete" class="btn btn-danger"><a href="delete.php?id='.$Id.'" class="text-light">Delete</a></button>
+     </td>
+    </tr>';
+    
+    }
+  }
+  ?>
+  </tbody>
+</table>
+     <button type="submit" name="Adduser" class="btn btn-primary my-5"><a href="Employee.php" class="text-light">Add User</button> 
+</div>
+</div>
+</div>
+</div>
+</body>
+
+</html>
