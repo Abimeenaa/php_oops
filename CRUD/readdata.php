e7e70115be2df71ff4eff56a8f20116e102d72e1
Abimeena <114210843+Abimeenaa@users.noreply.github.com>
2022-12-01 11:31:18 +0530

    Update readdata.php

--- CRUD/readdata.php
+++ CRUD/readdata.php
@@ -1,25 +1,19 @@
 <?php 
-    // $connection = mysqli_connect('localhost','root','','registration_web');
+   
      $connection = mysqli_connect('localhost','root','','registration_web');
      if(!$connection){ 
         die("OOPS!! CONNECTION FAILED!".mysqli_connect_error());
      }
      echo"CONNECTED SUCCESSFULLY!<br>";
      $query= "SELECT * FROM customerdata"; 
-    //$query = "SELECT * FROM signup";
+   
     $result = mysqli_query($connection,$query);
-    
-    //while($row = mysqli_fetch_all($result)){
+   
         $row = mysqli_fetch_all($result,MYSQLI_ASSOC);
         echo "<pre>";
         print_r($row);
         echo "</pre>";
-       //echo "NAME:".$row[0] ."<br>";
-        //echo "AGE:".$row[1]. "<br>";
-       //echo "PASSWORD:".$row[2]. "<br>";
-       //print_r ($row);
-    //}
-    // another approach
+      
     if(!$result){
        die("Query failed".mysqli_error($connection));
     }
